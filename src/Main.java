97b1e37565a8545083f44e83f37f0eba05d9a241
ethan-berman <eberman99@gmail.com>
2018-02-14 08:17:44 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Main extends Applet{
+
+	public void init(){
+		setSize(1024, 768);
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+}
