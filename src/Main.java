9bf24134ff81f13ed121b370ecb0beb6a9c07eed
Brando <brandon_li19@milton.edu>
2018-02-14 08:35:51 -0500

    Berman is ever stupider

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
-import java.applet.Applet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.FileWriter;
+import javax.swing.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.File;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.util.*;
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
 
-public class Main extends Applet{
 
-	public void init(){
-		setSize(1024, 768);
-	}
-	
-	public void paint(Graphics g){
-		
-	}
+public class Main extends JFrame implements ActionListener{
+
+		public static void main(String[] args) {
+			//System.out.println("Read...");
+			Main m = new Main();
+
+
+		}
+
+		public void actionPerformed(ActionEvent e) {
+
+			System.out.println("Button clicked  " + e.getActionCommand());
+
+
+		}
 }
