579cb40ac7a0dabd702662f60cd1662088d0216c
Brando <brandon_li19@milton.edu>
2018-02-26 09:51:23 -0500

    Carried the team

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,7 @@ public class Main extends JFrame implements ActionListener{
 		String supply;
 		static int quantity_bound;
 		int supply_bound;
+		//public String petStrings[];
 		public static void main(String[] args) {
 			//System.out.println("Read...");
 			Main m = new Main();
@@ -42,7 +43,7 @@ public class Main extends JFrame implements ActionListener{
 			//format for input of equations is k*math.pow(x,eps) 
 			Equation line = new Equation("5*Math.pow(x,-1)",m);
 			for(double i=0.0;i<line.graph(line.curve).length;i += .25){
-				System.out.println(line.graph(line.curve)[(int) (i/.25)]);
+				//System.out.println(line.graph(line.curve)[(int) (i/.25)]);
 			}
 			//System.out.println(line.integral()[0]);
 			
@@ -60,7 +61,34 @@ public class Main extends JFrame implements ActionListener{
 			chartpanel.setDomainZoomable(true);
 			add(chartpanel,BorderLayout.CENTER);
 
+			String[] commodity = { "Tobacco", "Footwear", "Jewelry", "Electricity", "Taxi", "Newspapers", "Movies" };
+			final JComboBox<String> commodityList = new JComboBox<String>(commodity);
+			commodityList.setSelectedIndex(0);
+			commodityList.addActionListener(this);
+			add(commodityList,BorderLayout.SOUTH);
+			String stringCommodity = (String) commodityList.getSelectedItem();
 
+			if(stringCommodity.equals("Tobacco")) {
+
+			}
+			if(stringCommodity.equals("Footwear")) {
+
+			}
+			if(stringCommodity.equals("Jewelry")) {
+
+			}
+			if(stringCommodity.equals("Electricity")) {
+
+			}
+			if(stringCommodity.equals("Taxi")) {
+
+			}
+			if(stringCommodity.equals("Newspapers")) {
+
+			}
+			if(stringCommodity.equals("Movies")) {
+
+			}
 
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setSize(600, 600);
@@ -76,6 +104,7 @@ public class Main extends JFrame implements ActionListener{
 
 			System.out.println("Button clicked  " + e.getActionCommand());
 
+			
 
 		}
 		
